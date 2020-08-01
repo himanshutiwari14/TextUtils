14348bf1311d0b2d30adfea630bd5cf5f0cd9930
Himanshu Tiwari <56720893+Himanshu-idexcel@users.noreply.github.com>
2020-08-01 09:40:23 +0530

    ecom food app initial commit

---
+++ FOODSITE/FoodSite/FoodtrackinApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FoodtrackinappConfig(AppConfig):
+    name = 'FoodtrackinApp'
---
+++ FOODSITE/FoodSite/myawsmCart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyawsmcartConfig(AppConfig):
+    name = 'myawsmCart'
